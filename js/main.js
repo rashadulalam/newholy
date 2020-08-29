4d084313091b09e829d778b55a7f2cd8f570f779
Rashadul Alam <rashadul91@gmail.com>
2020-08-29 19:34:41 +0600

    News Box

---
+++ js/main.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function() {
+    jQuery('#newsbox-item').marquee({
+      //duration in milliseconds of the marquee
+	duration: 5000,
+	//gap in pixels between the tickers
+	gap: 100,
+	//time in milliseconds before the marquee will start animating
+	delayBeforeStart: 0,
+	//'left' or 'right'
+	direction: 'left',
+	//true or false - should the marquee be duplicated to show an effect of continues flow
+	pauseOnHover: true,
+    });
+});
